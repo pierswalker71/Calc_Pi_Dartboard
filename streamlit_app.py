57d6d7c8557b9b8ba23e9601fede3cec4b469de9
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-06-19 11:43:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,14 +49,11 @@ def main():
     ax.scatter(x, y,marker='.', c=colour, s=15)
 
     # Calculate and display pi value
-    plt.rcParams['text.usetex'] = True
-    ax.set_title(r'\pi \approx '+str(np.round(4*sum(number_in_circle)/max_num,3)))
+    #plt.rcParams['text.usetex'] = True
+    #ax.set_title(r'\pi \approx '+str(np.round(4*sum(number_in_circle)/max_num,3)))
     
-    #pi_text = str(np.round(4*sum(number_in_circle)/max_num,3))
-    #st.latex(r'''
-    #         \pi \approx pi_text 
-    #         ''')
-
+    ax.set_title('pi approx '+str(np.round(4*sum(number_in_circle)/max_num,3)))
+    
     # Plot circle outline
     circle = plt.Circle((0.5, 0.5), 0.5, fill=False, color='0.5')
     ax.add_artist(circle)
